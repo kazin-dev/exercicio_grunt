0e6d121119f33a342fc7f2c5ceb2b9f207f636a8
kauã gomes <kgs26121967@gmail.com>
2024-02-19 19:01:09 -0300

    commit inicial

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less',
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*'],
+                tasks: ['less:development']
+            }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: "ENDERECO_DO_CSS",
+                            replacement: './styles/main.css'
+                        },
+                        {
+                            match: "ENDERECO_DO_JS",
+                            replacement: '../src/scripts/main.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: "ENDERECO_DO_CSS",
+                            replacement: './styles/main.css'
+                        },
+                        {
+                            match: "ENDERECO_DO_JS",
+                            replacement: '../src/scripts/main.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['less:development', 'watch']);
+    grunt.registerTask('build', ['less:production', 'replace:dist', 'uglify']);
+};
